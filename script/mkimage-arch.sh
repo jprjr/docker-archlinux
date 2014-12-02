d14b837e4cb71f88acfca5d6ee9b4ece1ed5cf08
John Regan <john@jrjrtech.com>
2014-12-02 08:25:30 -0600

    gpg-agent left running
    This should fix issue #1

--- script/mkimage-arch.sh
+++ script/mkimage-arch.sh
@@ -59,7 +59,7 @@ EOF
 
 touch $ROOTFS/etc/resolv.conf
 
-arch-chroot $ROOTFS /bin/sh -c "haveged -w 1024; pacman-key --init; pkill haveged; pacman -Rs --noconfirm haveged; pacman-key --populate archlinux"
+arch-chroot $ROOTFS /bin/sh -c "haveged -w 1024; pacman-key --init; pkill haveged; pacman -Rs --noconfirm haveged; pacman-key --populate archlinux; pkill gpg-agent"
 arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/UTC /etc/localtime"
 echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
 arch-chroot $ROOTFS locale-gen
